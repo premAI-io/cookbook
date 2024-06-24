8c716709e980735625c4d91f19be918e3b5c8ba2
Anindyadeep <proanindyadeep@gmail.com>
2024-06-24 18:22:41 +0530

    Add database utilities

---
+++ chat-with-sql/db_utils.py
@@ -0,0 +1,48 @@
+from typing import Union
+from sqlalchemy import MetaData, create_engine
+
+from llama_index.core import SQLDatabase
+from llama_index.core.query_engine import NLSQLTableQueryEngine
+from llama_index.core.llms.llm import LLM
+from llama_index.core.embeddings import BaseEmbedding
+
+
+def get_all_tables_from_db(db_config: dict) -> Union[list, None]:
+    metadata = MetaData()
+    username = db_config["username"]
+    host = db_config["host"]
+    password = db_config["password"]
+    port = db_config["port"]
+    database = db_config["database"]
+
+    try:
+        engine = create_engine(
+            f"postgresql://{username}:{password}@{host}:{port}/{database}"
+        )
+        metadata.reflect(bind=engine)
+        table_names = metadata.tables.keys()
+    except Exception as e:
+        print(f"Error: {e}")
+        return None
+    return table_names
+
+
+def setup_index_before_chat(
+    db_config: dict, table: str, llm: LLM, embed_model: BaseEmbedding
+):
+    username = db_config["username"]
+    host = db_config["host"]
+    password = db_config["password"]
+    port = db_config["port"]
+    database = db_config["database"]
+    engine = create_engine(
+        f"postgresql://{username}:{password}@{host}:{port}/{database}"
+    )
+
+    sql_database = SQLDatabase(engine=engine, include_tables=[table])
+    query_engine = NLSQLTableQueryEngine(
+        sql_database=sql_database,
+        llm=llm,
+        embed_model=embed_model,
+    )
+    return query_engine
